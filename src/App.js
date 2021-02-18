fa2c53b69d16a258dc2abb8323e2562d398ce9f5
muriithi-ian <imuriithiian@gmail.com>
2021-02-18 17:09:08 +0300

    Implemented routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Home} />
+          <Route path='/About' component={About} />
+          <Route path='/contact' component={Contact} />
 
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = (props) => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, obcaecati provident, reprehenderit, dolorem odio minima at laboriosam repudiandae sequi debitis aperiam! Quisquam, beatae voluptates? Obcaecati animi eius suscipit minima consectetur!</p>
+        </div>
+    )
+}
+
+export default Contact
